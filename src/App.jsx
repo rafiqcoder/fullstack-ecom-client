c77f70fe0b96f60f773a8fffb206dd2aa7db9309
Rafiqul Islam <rafiqcoder@gmail.com>
2025-05-16 20:18:38 +0600

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import Layout from './components/Layout'
-import Home from './pages/Home'
-import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import NotFound from "./components/NotFound";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
@@ -17,12 +19,13 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
-
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
-     
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;

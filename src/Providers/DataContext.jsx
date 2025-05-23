64a88f3d53b090a8cc64ea1ce19a3dabf2c65af6
Rafiqul Islam <rafiqcoder@gmail.com>
2025-05-23 21:07:50 +0600

    database diagram , crud

--- src/Providers/DataContext.jsx
+++ src/Providers/DataContext.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export const DataProvider = React.createContext();
 
 const DataContext = ({ children }) => {
   const [data, setData] = React.useState("test result");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/products");
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data || data.length === 0) {
+    return <div>Loading...</div>;
+  }
+  console.log("Data fetched:", data);
 
   const values = {
     data,
